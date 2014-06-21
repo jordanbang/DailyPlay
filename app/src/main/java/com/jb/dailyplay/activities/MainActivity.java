ce1569e4498e9e11c2ef07ee1892cd8c7212d07a
Jordan Bangia <jordan.bangia@gmail.com>
2014-06-21 00:17:33 -0400

    Tried to set up ui thread updates, did not work

--- app/src/main/java/com/jb/dailyplay/activities/MainActivity.java
+++ app/src/main/java/com/jb/dailyplay/activities/MainActivity.java
@@ -3,10 +3,12 @@ package com.jb.dailyplay.activities;
 import android.accounts.AccountManager;
 import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.google.android.gms.auth.GoogleAuthUtil;
 import com.google.android.gms.common.AccountPicker;
@@ -21,14 +23,15 @@ public class MainActivity extends Activity {
     private static final String TAG = "MainActivity";
     private String mAccountName;
     private String mAuthToken;
+    private TextView mUpdateTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        chooseGoogleAccount();
+        mUpdateTextView = (TextView) findViewById(R.id.update);
 
-        music();
+        new GetDailyPlayMusicTask().execute();
     }
 
     public void chooseGoogleAccount() {
@@ -55,30 +58,41 @@ public class MainActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void music() {
-        Thread thread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                String password = "***REMOVED***";
-                String username = "george.doe231@gmail.com";
-                mDailyMusicManager = new DailyMusicManager();
-                mDailyMusicManager.login(username, password);
-                mDailyMusicManager.getRandomSongs(5, getBaseContext());
-            }
-        });
-        thread.start();
-    }
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        if (requestCode == PICK_ACCOUNT_REQUEST && resultCode == RESULT_OK) {
+//            mAccountName = data.getStringExtra(AccountManager.KEY_ACCOUNT_NAME);
+//            try {
+//                mAuthToken = GoogleAuthUtil.getToken(this, mAccountName, null);
+//            } catch (Exception e) {
+//                Log.e(TAG, "getting auth token failed");
+//            }
+//        }
+//        super.onActivityResult(requestCode, resultCode, data);
+//    }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == PICK_ACCOUNT_REQUEST && resultCode == RESULT_OK) {
-            mAccountName = data.getStringExtra(AccountManager.KEY_ACCOUNT_NAME);
-            try {
-                mAuthToken = GoogleAuthUtil.getToken(this, mAccountName, null);
-            } catch (Exception e) {
-                Log.e(TAG, "getting auth token failed");
-            }
+    private class GetDailyPlayMusicTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mUpdateTextView.setText("Starting to get DailyPlay list");
         }
-        super.onActivityResult(requestCode, resultCode, data);
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            String password = "***REMOVED***";
+            String username = "george.doe231@gmail.com";
+            mDailyMusicManager = new DailyMusicManager();
+            mDailyMusicManager.login(username, password);
+            mDailyMusicManager.getRandomSongs(5, getBaseContext());
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+            super.onProgressUpdate(values);
+            mUpdateTextView.setText("Finished getting DailyPlay list");
+        }
     }
 }
