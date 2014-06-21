123c6e010f9abe9ad2c5263d7d0a6dcc9e5a611e
Jordan Bangia <george.doe231@gmail.com>
2014-06-21 00:57:18 -0400

    Added shared pref utils and String utils (should think about making this stuff a library)

---
+++ app/src/main/java/com/jb/dailyplay/utils/SharedPref.java
@@ -0,0 +1,40 @@
+package com.jb.dailyplay.utils;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.SharedPreferences;
+
+public class SharedPref {
+    public static SharedPreferences settings;
+    public static SharedPreferences.Editor editor;
+
+    @SuppressLint("CommitPrefEdits")
+    public static void initSharedPref(Activity current, String prefName){
+        if (settings == null) {
+            settings = current.getSharedPreferences(prefName,0);
+            editor = settings.edit();
+        }
+    }
+
+    public static String getString(String VarName, String DefValue) {
+        return settings.getString(VarName, DefValue);
+    }
+
+    public static void setString(String VarName, String nValue) {
+        editor.putString(VarName, nValue);
+        editor.commit();
+    }
+
+    public static void setInt(String VarName, int val) {
+        editor.putInt(VarName, val);
+        editor.commit();
+    }
+
+    public static int getInt(String VarName, int DefValue) {
+        return settings.getInt(VarName, DefValue);
+    }
+
+    public static String getString(String VarName) {
+        return getString(VarName, "");
+    }
+}
